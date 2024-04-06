eefd65901d594fb00c931bf17dd2e30ef2360575
Natsumi <nat.k@kino41.local>
2024-04-06 22:34:24 +0900

    夜の挨拶に変更

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Good morning natsumi!");
+        System.out.println("Good evening natsumi!");
     }
 }
